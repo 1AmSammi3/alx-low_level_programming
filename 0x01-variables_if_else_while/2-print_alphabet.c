0aa80fe4198072f356b716141dacc925742b54a6
Sammie <osenisamad2004@gmail.com>
2023-03-17 07:56:45 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code
+ *
+ * Return: Code returns 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
