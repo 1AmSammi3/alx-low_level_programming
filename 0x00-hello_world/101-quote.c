6d78d05e038d6584e15f3e434b8c6aef212ece15
Sammie <osenisamad2004@gmail.com>
2023-03-16 22:56:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is an entry point
+ *
+ * Return: This returns 1
+ */
+
+int main(void)
+{
+	fwrite(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
